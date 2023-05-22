1fe061b713db67d8da39538b20b9b23bef668411
Marquise Campbell <marquisetyrrell@gmail.com>
2023-05-22 15:06:48 -0400

    phase 8

--- Program.cs
+++ Program.cs
@@ -1,4 +1,123 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System;
+Main();
+
+void Main()
+{
 Console.WriteLine("Welcome to the Enthusiastic Moose Simulator!");
 Console.WriteLine("--------------------------------------------");
+Console.WriteLine();
 
+// Let the moose speak!
+MooseSays("H I, I'M  E N T H U S I A S T I C !");
+MooseSays("I really am enthusiastic");
+
+// Ask a question
+CanadaQuestion();
+SadQuestion();
+LoveCSharpQuestion();
+SnowQuestion();
+}
+
+void CanadaQuestion()
+{
+bool isTrue = MooseAsks("Is Canada real?");
+if (isTrue)
+{
+    MooseSays("Really? It seems very unlikely.");
+}
+else
+{
+    MooseSays("I  K N E W  I T !!!");
+}
+}
+
+void SadQuestion()
+{
+    bool isSad = MooseAsks("Are you Sad?");
+    if (isSad){
+        MooseSays("Cheer up butter cup"
+        );
+    }
+    else {
+        MooseSays("Spread those good thoughts around");
+    }
+}
+
+void LoveCSharpQuestion()
+{
+    bool doesLoveCSharp = MooseAsks("Do you love C# yet?");
+    if (doesLoveCSharp)
+    {
+        MooseSays("Good job sucking up to your instructor!");
+    }
+    else
+    {
+        MooseSays("You will...oh, yes, you will...");
+    }
+}
+
+void SnowQuestion()
+{
+    bool wantSnow = MooseAsks("Do you wanna build a snowman?");
+    if (wantSnow)
+    {
+        MooseSays("Come on let's go out and play");
+    }
+
+    else{
+        MooseSays("Snow Melts too fast :(");
+    }
+}
+
+bool MooseAsks(string question)
+{
+    Console.Write($"{question} (Y/N): ");
+    string answer = Console.ReadLine().ToLower();
+
+    while (answer != "y" && answer != "n")
+    {
+        Console.Write($"{question} (Y/N): ");
+        answer = Console.ReadLine().ToLower();
+    }
+
+    if (answer == "y")
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+void MooseSays(string message)
+{
+    Console.WriteLine($@"
+                                       _.--^^^--,
+                                    .'          `\
+  .-^^^^^^-.                      .'              |
+ /          '.                   /            .-._/
+|             `.                |             |
+ \              \          .-._ |          _   \
+  `^^'-.         \_.-.     \   `          ( \__/
+        |             )     '=.       .,   \
+       /             (         \     /  \  /
+     /`               `\        |   /    `'
+     '..-`\        _.-. `\ _.__/   .=.
+          |  _    / \  '.-`    `-.'  /
+          \_/ |  |   './ _     _  \.'
+               '-'    | /       \ |
+                      |  .-. .-.  |
+                      \ / o| |o \ /
+                       |   / \   |    {message}
+                      / `^`   `^` \
+                     /             \
+                    | '._.'         \
+                    |  /             |
+                     \ |             |
+                      ||    _    _   /
+                      /|\  (_\  /_) /
+                      \ \'._  ` '_.'
+                       `^^` `^^^`
+    ");
+}
